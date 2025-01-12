9f570522a5fabb269e74ba23e172699adf9166d2
Linus Kåreblom <linus.kareblom@gmail.com>
2025-01-12 15:40:11 +0100

    I really have no idea how inline works.

--- waddle.h
+++ waddle.h
@@ -44,7 +44,7 @@
 
 #ifdef WDL_POSIX
 #define WDL_THREAD_LOCAL __thread
-#define WDL_INLINE inline __attribute__((always_inline))
+#define WDL_INLINE __attribute__((always_inline))
 #endif // WDL_POSIX
 
 typedef unsigned char      u8;
@@ -487,22 +487,22 @@ char* wdl_str_to_cstr(WDL_Arena* arena, WDL_Str str)  {
 //
 // Vec2
 //
-WDL_Vec2 wdl_v2(f32 x, f32 y) { return (WDL_Vec2) {x, y}; }
-WDL_Vec2 wdl_v2s(f32 scaler) { return (WDL_Vec2) {scaler, scaler}; }
+inline WDL_Vec2 wdl_v2(f32 x, f32 y) { return (WDL_Vec2) {x, y}; }
+inline WDL_Vec2 wdl_v2s(f32 scaler) { return (WDL_Vec2) {scaler, scaler}; }
 
-WDL_Vec2 wdl_v2_add(WDL_Vec2 a, WDL_Vec2 b) { return wdl_v2(a.x + b.x, a.y + b.y); }
-WDL_Vec2 wdl_v2_sub(WDL_Vec2 a, WDL_Vec2 b) { return wdl_v2(a.x - b.x, a.y - b.y); }
-WDL_Vec2 wdl_v2_mul(WDL_Vec2 a, WDL_Vec2 b) { return wdl_v2(a.x * b.x, a.y * b.y); }
-WDL_Vec2 wdl_v2_div(WDL_Vec2 a, WDL_Vec2 b) { return wdl_v2(a.x / b.x, a.y / b.y); }
+inline WDL_Vec2 wdl_v2_add(WDL_Vec2 a, WDL_Vec2 b) { return wdl_v2(a.x + b.x, a.y + b.y); }
+inline WDL_Vec2 wdl_v2_sub(WDL_Vec2 a, WDL_Vec2 b) { return wdl_v2(a.x - b.x, a.y - b.y); }
+inline WDL_Vec2 wdl_v2_mul(WDL_Vec2 a, WDL_Vec2 b) { return wdl_v2(a.x * b.x, a.y * b.y); }
+inline WDL_Vec2 wdl_v2_div(WDL_Vec2 a, WDL_Vec2 b) { return wdl_v2(a.x / b.x, a.y / b.y); }
 
-WDL_Vec2 wdl_v2_adds(WDL_Vec2 vec, f32 scaler) { return wdl_v2(vec.x + scaler, vec.y + scaler); }
-WDL_Vec2 wdl_v2_subs(WDL_Vec2 vec, f32 scaler) { return wdl_v2(vec.x - scaler, vec.y - scaler); }
-WDL_Vec2 wdl_v2_muls(WDL_Vec2 vec, f32 scaler) { return wdl_v2(vec.x * scaler, vec.y * scaler); }
-WDL_Vec2 wdl_v2_divs(WDL_Vec2 vec, f32 scaler) { return wdl_v2(vec.x / scaler, vec.y / scaler); }
+inline WDL_Vec2 wdl_v2_adds(WDL_Vec2 vec, f32 scaler) { return wdl_v2(vec.x + scaler, vec.y + scaler); }
+inline WDL_Vec2 wdl_v2_subs(WDL_Vec2 vec, f32 scaler) { return wdl_v2(vec.x - scaler, vec.y - scaler); }
+inline WDL_Vec2 wdl_v2_muls(WDL_Vec2 vec, f32 scaler) { return wdl_v2(vec.x * scaler, vec.y * scaler); }
+inline WDL_Vec2 wdl_v2_divs(WDL_Vec2 vec, f32 scaler) { return wdl_v2(vec.x / scaler, vec.y / scaler); }
 
-f32 wdl_v2_magnitude_squared(WDL_Vec2 vec) { return vec.x * vec.x + vec.y * vec.y; }
-f32 wdl_v2_magnitude(WDL_Vec2 vec) { return sqrtf(wdl_v2_magnitude_squared(vec)); }
-WDL_Vec2 wdl_v2_normalized(WDL_Vec2 vec) {
+inline f32 wdl_v2_magnitude_squared(WDL_Vec2 vec) { return vec.x * vec.x + vec.y * vec.y; }
+inline f32 wdl_v2_magnitude(WDL_Vec2 vec) { return sqrtf(wdl_v2_magnitude_squared(vec)); }
+inline WDL_Vec2 wdl_v2_normalized(WDL_Vec2 vec) {
     f32 inv_mag = 1.0f / wdl_v2_magnitude(vec);
     return wdl_v2_muls(vec, inv_mag);
 }
@@ -510,21 +510,21 @@ WDL_Vec2 wdl_v2_normalized(WDL_Vec2 vec) {
 //
 // Ivec2
 //
-WDL_Ivec2 wdl_iv2(i32 x, i32 y) { return (WDL_Ivec2) {x, y}; }
-WDL_Ivec2 wdl_iv2s(i32 scaler) { return (WDL_Ivec2) {scaler, scaler}; }
+inline WDL_Ivec2 wdl_iv2(i32 x, i32 y) { return (WDL_Ivec2) {x, y}; }
+inline WDL_Ivec2 wdl_iv2s(i32 scaler) { return (WDL_Ivec2) {scaler, scaler}; }
 
-WDL_Ivec2 wdl_iv2_add(WDL_Ivec2 a, WDL_Ivec2 b) { return wdl_iv2(a.x + b.x, a.y + b.y); }
-WDL_Ivec2 wdl_iv2_sub(WDL_Ivec2 a, WDL_Ivec2 b) { return wdl_iv2(a.x - b.x, a.y - b.y); }
-WDL_Ivec2 wdl_iv2_mul(WDL_Ivec2 a, WDL_Ivec2 b) { return wdl_iv2(a.x * b.x, a.y * b.y); }
-WDL_Ivec2 wdl_iv2_div(WDL_Ivec2 a, WDL_Ivec2 b) { return wdl_iv2(a.x / b.x, a.y / b.y); }
+inline WDL_Ivec2 wdl_iv2_add(WDL_Ivec2 a, WDL_Ivec2 b) { return wdl_iv2(a.x + b.x, a.y + b.y); }
+inline WDL_Ivec2 wdl_iv2_sub(WDL_Ivec2 a, WDL_Ivec2 b) { return wdl_iv2(a.x - b.x, a.y - b.y); }
+inline WDL_Ivec2 wdl_iv2_mul(WDL_Ivec2 a, WDL_Ivec2 b) { return wdl_iv2(a.x * b.x, a.y * b.y); }
+inline WDL_Ivec2 wdl_iv2_div(WDL_Ivec2 a, WDL_Ivec2 b) { return wdl_iv2(a.x / b.x, a.y / b.y); }
 
-WDL_Ivec2 wdl_iv2_adds(WDL_Ivec2 vec, i32 scaler) { return wdl_iv2(vec.x + scaler, vec.y + scaler); }
-WDL_Ivec2 wdl_iv2_subs(WDL_Ivec2 vec, i32 scaler) { return wdl_iv2(vec.x - scaler, vec.y - scaler); }
-WDL_Ivec2 wdl_iv2_muls(WDL_Ivec2 vec, i32 scaler) { return wdl_iv2(vec.x * scaler, vec.y * scaler); }
-WDL_Ivec2 wdl_iv2_divs(WDL_Ivec2 vec, i32 scaler) { return wdl_iv2(vec.x / scaler, vec.y / scaler); }
+inline WDL_Ivec2 wdl_iv2_adds(WDL_Ivec2 vec, i32 scaler) { return wdl_iv2(vec.x + scaler, vec.y + scaler); }
+inline WDL_Ivec2 wdl_iv2_subs(WDL_Ivec2 vec, i32 scaler) { return wdl_iv2(vec.x - scaler, vec.y - scaler); }
+inline WDL_Ivec2 wdl_iv2_muls(WDL_Ivec2 vec, i32 scaler) { return wdl_iv2(vec.x * scaler, vec.y * scaler); }
+inline WDL_Ivec2 wdl_iv2_divs(WDL_Ivec2 vec, i32 scaler) { return wdl_iv2(vec.x / scaler, vec.y / scaler); }
 
-f32 wdl_iv2_magnitude_squared(WDL_Ivec2 vec) { return vec.x * vec.x + vec.y * vec.y; }
-f32 wdl_iv2_magnitude(WDL_Ivec2 vec) { return sqrtf(wdl_iv2_magnitude_squared(vec)); }
+inline f32 wdl_iv2_magnitude_squared(WDL_Ivec2 vec) { return vec.x * vec.x + vec.y * vec.y; }
+inline f32 wdl_iv2_magnitude(WDL_Ivec2 vec) { return sqrtf(wdl_iv2_magnitude_squared(vec)); }
 
 // -- OS -----------------------------------------------------------------------
 // Platform specific implementation
